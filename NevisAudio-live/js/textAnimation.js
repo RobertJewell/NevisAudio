41d09aa81d66f37a69ce2d39914cb0ece5dcc4f9
RobertJewell <rjewellaudio@gmail.com>
2020-07-21 12:32:45 +0200

    completed text animation

--- NevisAudio-live/js/textAnimation.js
+++ NevisAudio-live/js/textAnimation.js
@@ -1,5 +1,29 @@
-let words = ["SEEN", "", "HEARD", "", "NOTICED"]
+let words = ["SEEN", "HEARD", "NOTICED"];
 
-let changeWord = document.querySelector(".landing__bannerText--changeWord")
+let changeWord = document.querySelector(".landing__bannerText--changeWord");
 
+// for (let i = 0; i < words.length; i++) {
+//     setTimeout(`changeWord.textContent = words[${i}]`, (i * 1000));
+// }
 
+for (let i = 0; i < words.length; i++) {
+  setTimeout(textChange, i * 1600, words[i]);
+}
+
+// changeWord.textContent = words[i]
+
+function textChange(word) {
+  changeWord.style.opacity = 0;
+  setTimeout(() => {
+    changeWord.textContent = word;
+    changeWord.setAttribute("data-content", word);
+  }, 400);
+  setTimeout(() => {
+    changeWord.style.opacity = 1;
+    console.log("1");
+  }, 800);
+}
+
+function ChangeWords(word) {
+  changeWord.setAttribute("data-content", `{$word}`);
+}
