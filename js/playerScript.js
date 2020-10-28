e4f7531da930d146a4fb933878011ff97a15740b
Robert Jewell <rjewellaudio@gmail.com>
2020-10-28 18:08:04 +0100

    added alert

--- js/playerScript.js
+++ js/playerScript.js
@@ -13,30 +13,31 @@ VARIABLES
 ---------------- 
 */
 
-// //set up audio context
-// const AudioContext = window.AudioContext || window.webkitAudioContext;
-// const audioContext = new AudioContext();
-// const audioElement = document.getElementById("audioPlayer");
-// const sourceTrack = audioContext.createMediaElementSource(audioElement);
-// const channelSplitter = audioContext.createChannelSplitter(4)
-// const mergeMixed = audioContext.createChannelMerger(2)
-// const mergeUnmixed = audioContext.createChannelMerger(2)
-// const mixedGain = audioContext.createGain()
-// const unmixedGain = audioContext.createGain()
+//set up audio context
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+const audioElement = document.getElementById("audioPlayer");
+const sourceTrack = audioContext.createMediaElementSource(audioElement);
+const channelSplitter = audioContext.createChannelSplitter(4)
+const mergeMixed = audioContext.createChannelMerger(2)
+const mergeUnmixed = audioContext.createChannelMerger(2)
+const mixedGain = audioContext.createGain()
+const unmixedGain = audioContext.createGain()
 
-// //Routing
-// sourceTrack.connect(channelSplitter);
-// channelSplitter.connect(mergeMixed, 0, 0);
-// channelSplitter.connect(mergeMixed, 1, 1);
-// channelSplitter.connect(mergeUnmixed, 2, 0);
-// channelSplitter.connect(mergeUnmixed, 3, 1);
-// mergeMixed.connect(mixedGain)
-// mergeUnmixed.connect(unmixedGain)
-// mixedGain.connect(audioContext.destination)
-// unmixedGain.connect(audioContext.destination)
+//Routing
+sourceTrack.connect(channelSplitter);
+channelSplitter.connect(mergeMixed, 0, 0);
+channelSplitter.connect(mergeMixed, 1, 1);
+channelSplitter.connect(mergeUnmixed, 2, 0);
+channelSplitter.connect(mergeUnmixed, 3, 1);
+mergeMixed.connect(mixedGain)
+mergeUnmixed.connect(unmixedGain)
+mixedGain.connect(audioContext.destination)
+unmixedGain.connect(audioContext.destination)
 
 //Set initial gain
-
+unmixedGain.gain.value = 0
+mixedGain.gain.value = 1
 
 
 //Pull the list of tracks from the DOM
@@ -127,33 +128,9 @@ function resetPlayPause() {
 
 //play pause audio
 function playPauseTrack() {
-  if (!audioContext) {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioContext = new AudioContext();
-    const audioElement = document.getElementById("audioPlayer");
-    const sourceTrack = audioContext.createMediaElementSource(audioElement);
-    const channelSplitter = audioContext.createChannelSplitter(4)
-    const mergeMixed = audioContext.createChannelMerger(2)
-    const mergeUnmixed = audioContext.createChannelMerger(2)
-    const mixedGain = audioContext.createGain()
-    const unmixedGain = audioContext.createGain()
-
-    //Routing
-    sourceTrack.connect(channelSplitter);
-    channelSplitter.connect(mergeMixed, 0, 0);
-    channelSplitter.connect(mergeMixed, 1, 1);
-    channelSplitter.connect(mergeUnmixed, 2, 0);
-    channelSplitter.connect(mergeUnmixed, 3, 1);
-    mergeMixed.connect(mixedGain)
-    mergeUnmixed.connect(unmixedGain)
-    mixedGain.connect(audioContext.destination)
-    unmixedGain.connect(audioContext.destination)
-
-    unmixedGain.gain.value = 0
-    mixedGain.gain.value = 1
-  }
   if (audioContext.state === "suspended") {
     audioContext.resume();
+    alert(audioContext.state)
   }
   if (playButton.classList.contains("noDisplay")) {
     audioElement.play();
@@ -204,4 +181,3 @@ function resetSelectedTrack(arr) {
     arr[i].classList.remove("trackSelector__active");
   }
 }
-
